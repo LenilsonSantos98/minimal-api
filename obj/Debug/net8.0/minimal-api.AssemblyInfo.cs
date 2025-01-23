b376253f7c5c257a00e94460000a83cffdc8f595
Lenilson Santos <lenilsonrodrigo123@gmail.com>
2025-01-23 11:49:19 -0300

    plug-ins entity framework core, tools, design e sqlserver

--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+986728cd0e66a7d6fdc7ccba0b8735e732d2796f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c52d03e8d10eecb4c599d2bea214beef807bcad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
