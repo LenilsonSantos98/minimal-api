e218e7bf7d1542945e855b50cd23a35e3ab4ebd4
Lenilson Santos <lenilsonrodrigo123@gmail.com>
2025-01-23 09:20:15 -0300

    criação do projeto

---
+++ Documents/CURSOS/Dio/Java/API-CT/API-CT/src/main/java/com/API/API_CT/ApiCtApplication.java
@@ -0,0 +1,13 @@
+package com.API.API_CT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCtApplication.class, args);
+	}
+
+}
---
+++ Documents/CURSOS/Dio/Java/API-CT/API-CT/src/test/java/com/API/API_CT/ApiCtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.API_CT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
