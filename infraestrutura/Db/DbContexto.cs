95aa5d43d6a192d1fbab7897a81daf9dcd66c6c8
Lenilson Santos <lenilsonrodrigo123@gmail.com>
2025-01-27 09:43:24 -0300

    classe adm e gerencia de acesso ao DB

--- infraestrutura/Db/DbContexto.cs
+++ infraestrutura/Db/DbContexto.cs
@@ -1,5 +1,13 @@
-namespace MinimalAPI.Infraestutura.Db;
-public class DbContexto
-{
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
 
+namespace MinimalAPI.Infraestutura.Db;
+public class DbContexto : DbContext
+{
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("string de conexão");
+    }
 }
