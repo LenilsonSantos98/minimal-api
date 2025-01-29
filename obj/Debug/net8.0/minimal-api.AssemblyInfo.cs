779d100401fed8255b793ff6d5e30b6f1697ea8d
Lenilson Santos <lenilsonrodrigo123@gmail.com>
2025-01-29 17:24:16 -0300

    GET para retornar um veiculo

--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28178cb44474ffdca47edbca9bf3f3dc1982a461")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+681e24065d637c89e61f53b853351ee4f7519a87")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
