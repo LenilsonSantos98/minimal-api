20679b753f9f3dca3db5c9f179ae8c5d0c0256ad
Lenilson Santos <lenilsonrodrigo123@gmail.com>
2025-01-30 17:15:20 -0300

    validacao ao cadastra e atualizar veiculo

--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f5e23ae4a0075dc24e6df1f9a46caf437291a6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d6cc9f7be9f07f681cca87682b38aae8954d37")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
